af324b87a505e2e0c676a35e87f7acd3a294ca6d
Xinlei Cao <lukecxl@gmail.com>
2013-05-18 15:03:16 -0400

    test

--- testSceneManagement/Classes/TitleScene.h
+++ testSceneManagement/Classes/TitleScene.h
@@ -1,4 +1,28 @@
 #ifndef __TITLE_SCENE_H__
 #define __TITLE_SCENE_H__
-
+#include "cocos2d.h"
+using namespace cocos2d;
+// =============================================
+// Scene
+class TitleScene:public CCScene
+{
+public:
+	TitleScene():_layer(NULL){}
+	~TitleScene(){}
+	bool init();
+	CREATE_FUNC(TitleScene);
+	CC_SYNTHESIZE_READONLY(TitleSceneLayer*, _layer, Layer);
+};
+// =============================================
+// Layer
+class TitleSceneLayer:public CCLayerColor
+{
+public:
+	TitleSceneLayer():_label(NULL){}
+	virtual ~TitleSceneLayer(){}
+	bool init();
+	CREATE_FUNC(TitleSceneLayer);
+	void done();
+	CC_SYNTHESIZE_READONLY(CCLabelTTF*,_label,Label);
+};
 #endif // !__TITLE_SCENE_H__
---
+++ testSceneManagement/readme.txt
@@ -0,0 +1,3 @@
+this project is used to test the scene management of cocos2d-x
+
+Seperate the CCScene and CCLayer and manage them
