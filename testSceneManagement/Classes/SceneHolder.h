efead765f78eab0e1d15bbccd741343b4b74a7c9
Xinlei Cao <lukecxl@gmail.com>
2013-05-18 14:41:15 -0400

    testing scene management, add project

---
+++ testSceneManagement/Classes/SceneHolder.h
@@ -0,0 +1,4 @@
+#ifndef __SCENE_HOLDER_H__
+#define __SCENE_HOLDER_H__
+
+#endif // !__SCENE_HOLDER_H__
---
+++ testSceneManagement/Classes/TitleScene.h
@@ -0,0 +1,4 @@
+#ifndef __TITLE_SCENE_H__
+#define __TITLE_SCENE_H__
+
+#endif // !__TITLE_SCENE_H__
