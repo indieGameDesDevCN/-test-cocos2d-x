19bdb9face8a35283c771911fe728734a2630198
Xinlei Cao <lukecxl@gmail.com>
2013-05-19 14:11:41 -0400

    test actions

--- testBasicActions/Classes/PlayScene.cpp
+++ testBasicActions/Classes/PlayScene.cpp
@@ -30,6 +30,26 @@ bool PlaySceneLayer::init(){
 		_label->setPosition(ccp(winSize.width/2,winSize.height/2));
 		this->addChild(_label);
 
+		// =======================================
+		// sprite
+		mShip = CCSprite::create("ship.png");
+		mShip->setPosition(ccp(winSize.width/2,winSize.height/2));
+		this->addChild(mShip,0);
+		
+		// CCJumpBy
+		mShip->runAction(CCJumpBy::create(8,ccp(200,0),100,5));
+		// CCJumpTo
+		//mShip->runAction(CCJumpTo::create(16,ccp(100,100),30,3));
+		// CCMoveBy
+		//mShip->runAction(CCMoveBy::create(16,ccp(0,-90)));
+		// CCMoveTo
+		//mShip->runAction(CCMoveTo::create(16,ccp(winSize.width,winSize.height)));
+		// CCRotateBy
+		mShip->runAction(CCRotateBy::create(5,180));
+		// CCRotateTo
+		//mShip->runAction(CCRotateTo::create(5,90));
+		// =======================================
+		// end the scene
 		//this->runAction(CCSequence::create(CCDelayTime::create(3),CCCallFunc::create(this, callfunc_selector(PlaySceneLayer::end)), NULL));
 		CCSize visibleSize = CCDirector::sharedDirector()->getVisibleSize();
 		CCPoint origin = CCDirector::sharedDirector()->getVisibleOrigin();
