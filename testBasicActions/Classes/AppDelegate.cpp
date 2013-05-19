e7f6c4732f7e1518a2f49c7320b12f991521e5d1
Xinlei Cao <lukecxl@gmail.com>
2013-05-18 20:12:51 -0400

    test

--- testBasicActions/Classes/AppDelegate.cpp
+++ testBasicActions/Classes/AppDelegate.cpp
@@ -1,8 +1,9 @@
 #include "cocos2d.h"
 #include "CCEGLView.h"
 #include "AppDelegate.h"
-#include "HelloWorldScene.h"
 #include "SimpleAudioEngine.h"
+
+#include "SceneHolder.h"
 
 using namespace CocosDenshion;
 
@@ -30,7 +31,7 @@ bool AppDelegate::applicationDidFinishLaunching()
     pDirector->setAnimationInterval(1.0 / 60);
 
     // create a scene. it's an autorelease object
-    CCScene *pScene = HelloWorld::scene();
+	CCScene *pScene = SceneHolder::initSceneByName(SceneHolder::SceneName::Title);
 
     // run
     pDirector->runWithScene(pScene);
